e18110e0739cacd0a1ef0bfc0ccb21b0e36d244a
James0822 <52530418+James0822@users.noreply.github.com>
2025-10-20 14:29:20 +0800

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,4 +1,5 @@
 # VPC
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
@@ -83,3 +84,4 @@ resource "aws_route_table_association" "data_private" {
 }
   
  
+
